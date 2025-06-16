312d5b560a57ba440d307e5623c9f973de85a63f
AndersonCango <118143904+AndersonCango@users.noreply.github.com>
2025-06-15 21:50:27 -0500

    feat: API fundamentals with nestjs - More examples

--- 02-server/example/src/app.controller.ts
+++ 02-server/example/src/app.controller.ts
@@ -7,6 +7,7 @@ import {
   Post,
   Body,
   HttpCode,
+  NotFoundException,
 } from '@nestjs/common';
 import { AppService } from './app.service';
 
@@ -25,8 +26,30 @@ export class AppController {
     @Param('id') id, // Parametro de Ruta llamado 'id'
     @Query('hola') hola, // Parametro de consulta llamado 'hola'
     @Headers('escuela') escuela, // Cabecera con nombre 'escuela'
-    @Body('monto') monto, // Cabecera con nombre 'escuela'
+    @Body('monto') monto, // Campo del cuerpo llamado 'monto'
   ): string {
     return id + hola + ' Funcionando ' + escuela + monto;
+  }
+
+  private casas = [
+    { id: 1, nombre: 'Casa 1' },
+    { id: 2, nombre: 'Casa 2' },
+  ];
+
+  @Get('/casa')
+  @HttpCode(200)
+  casa(@Query('idCasa') idCasa?: string) {
+    if (!idCasa) {
+      return this.casas;
+    }
+
+    const id = parseInt(idCasa, 10);
+    const casa = this.casas.find((c) => c.id === id);
+
+    if (!casa) {
+      throw new NotFoundException('No se encuentra');
+    }
+
+    return [casa];
   }
 }
