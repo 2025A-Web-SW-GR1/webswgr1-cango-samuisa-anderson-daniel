bbb33140b39e21919ffee7a4f6a84a1d87f405e4
AndersonCango <118143904+AndersonCango@users.noreply.github.com>
2025-07-29 07:17:59 -0500

    feat: add own npm packages

---
+++ packages/index.js
@@ -0,0 +1,7 @@
+const paqueteSuma = require("2025a-swgr1-adcs-suma");
+const respuestaSuma = paqueteSuma.suma(4, 9);
+console.log("La respuesta de la suma es: ", respuestaSuma);
+
+const paqueteResta = require("2025a-swgr1-adcs-resta");
+const respuestaResta = paqueteResta.resta(24, 9);
+console.log("La respuesta de la resta es: ", respuestaResta);
